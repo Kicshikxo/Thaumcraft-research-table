4729e3a3b7323d09509937022475257fed998799
Kicshikxo <supermegavalera9000@gmail.com>
2022-01-03 01:42:01 +0300

    added @electron/remote initialize

--- main.js
+++ main.js
@@ -1,53 +1,57 @@
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron')
+const { initialize: initializeRemote, enable: enableRemote } = require('@electron/remote/main')
 const path = require('path')
+initializeRemote()
 
-function createWindow () {
-  const mainWindow = new BrowserWindow({
-	frame: false,
-	
-	width: 768,
-	minWidth: 768,
-	maxWidth: 768,
-	
-	height: 530,
-	minHeight: 530,
-	maxHeight: 530,
-	
-	backgroundColor: '#161616',
-	
-// 	show: false, // Для отображения только после загрузки
-	
-	resizable: false,
-	
-    webPreferences: {
-		devTools: false,
-      preload: path.join(__dirname, 'preload.js'),
-	    enableRemoteModule : true,
-		contextIsolation : true,
-		sandbox: true
-    },
-    
-    icon: __dirname+'/icons/icon.png',
-  })
+function createWindow() {
+	const mainWindow = new BrowserWindow({
+		frame: false,
 
-  mainWindow.loadFile('index.html')
-  
+		width: 768,
+		minWidth: 768,
+		maxWidth: 768,
+
+		height: 530,
+		minHeight: 530,
+		maxHeight: 530,
+
+		backgroundColor: '#161616',
+
+		// 	show: false, // Для отображения только после загрузки
+
+		resizable: false,
+
+		webPreferences: {
+			// sandbox: true, // breaks @electron/remote import
+			devTools: false,
+			preload: path.join(__dirname, 'preload.js'),
+			enableRemoteModule: true,
+			contextIsolation: true
+		},
+
+		icon: path.join(__dirname, '/icons/icon.png')
+	})
+
+	mainWindow.loadFile('index.html')
+
 	// Отображение окна только после загрузки
-// 	mainWindow.once('ready-to-show', () => {
-// 		mainWindow.show() 
-// 	})
+	// 	mainWindow.once('ready-to-show', () => {
+	// 		mainWindow.show() 
+	// 	})
 
-  // mainWindow.webContents.openDevTools()
+	// mainWindow.webContents.openDevTools()
+
+	enableRemote(mainWindow.webContents)
 }
 
 app.whenReady().then(() => {
-  createWindow()
-  
-  app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
+	createWindow()
+
+	app.on('activate', function () {
+		if (BrowserWindow.getAllWindows().length === 0) createWindow()
+	})
 })
 
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+	if (process.platform !== 'darwin') app.quit()
 })
