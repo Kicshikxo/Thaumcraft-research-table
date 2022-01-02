2c000ff511bc1aa63592d8ef0b62b035ab0cf29b
Kicshikxo <supermegavalera9000@gmail.com>
2022-01-03 01:38:56 +0300

    fixed window.control methods

--- preload.js
+++ preload.js
@@ -1,13 +1,14 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 
-const { contextBridge, remote } = require('electron')
+const { contextBridge } = require('electron')
+const { BrowserWindow } = require('@electron/remote')
 
 contextBridge.exposeInMainWorld('control', {
 	minimize: () => {
-		remote.BrowserWindow.getFocusedWindow().minimize()
+		BrowserWindow.getFocusedWindow().minimize()
 	},
 	close: () => {
-		remote.BrowserWindow.getFocusedWindow().close()
+		BrowserWindow.getFocusedWindow().close()
 	}
 })
